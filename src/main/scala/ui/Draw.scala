5d90cbbb11fd7b3674c63d74c690b9b7afafaa7d
sshipika <Sshilpika5@gmail.com>
2014-10-08 15:02:57 -0500

    Size.scala additions + Draw.scala

--- src/main/scala/ui/Draw.scala
+++ src/main/scala/ui/Draw.scala
@@ -19,21 +19,28 @@ class Draw(canvas: Canvas, paint: Paint) {
   // TODO entirely your job (except Circle)
 
   def apply(s: Shape): Unit = s match {
-    case Circle(radius) => canvas.drawCircle(0, 0, radius, paint)
-    case Rectangle(width, height) => canvas.drawRect(0, 0, width, height, paint)
-    case Polygon(p1, p2, p3, p4) => canvas.drawLines(Array[Float](p1.x, p1.y, p2.x, p2.y, p2.x, p2.y, p3.x, p3.y, p3.x, p3.y, p4.x, p4.y, p1.x, p1.y, p4.x, p4.y), paint)
-    case Fill(shape) => paint.setStyle(Style.FILL)
+    case Circle(radius) =>
+              canvas.drawCircle(0, 0, radius, paint)
+    case Rectangle(width, height) =>
+              canvas.drawRect(0, 0, width, height, paint)
+    case Polygon(p1, p2, p3, p4) =>
+              canvas.drawLines(Array[Float](p1.x, p1.y, p2.x, p2.y, p2.x, p2.y, p3.x, p3.y, p3.x, p3.y, p4.x, p4.y, p1.x, p1.y, p4.x, p4.y), paint)
+    case Fill(shape) =>
+              paint.setStyle(Style.FILL_AND_STROKE)
               apply(shape)
+              paint.setStyle(Style.STROKE)
     case Location(width, height, shape) =>
               canvas.translate(width, height)
               apply(shape)
               canvas.translate(-width, -height)
-    case Outline(shape) => paint.setStyle(Style.STROKE)
-              apply(shape)
-    case Stroke(color, shape) =>
-              paint.setColor(color)
+    case Outline(shape) =>
               paint.setStyle(Style.STROKE)
               apply(shape)
+              paint.setStyle(Style.STROKE)
+    case Stroke(color, shape) =>
+              paint.setColor(color)
+              apply(shape)
+              paint.setColor(Color.BLACK)
     case Group(shapeList) =>
               for (shape:Shape <- shapeList) {
                 apply(shape)
