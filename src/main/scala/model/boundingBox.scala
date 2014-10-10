904b6c455987c462b826c5124c6a9f90cef7a2e0
sshipika <Sshilpika5@gmail.com>
2014-10-09 23:10:55 -0500

    Points varargs + Bounding Box + size

--- src/main/scala/model/boundingBox.scala
+++ src/main/scala/model/boundingBox.scala
@@ -7,31 +7,69 @@ package edu.luc.etl.cs313.scala.shapes.model
  */
 object boundingBox {
 
-  // TODO entirely your job (except Circle)
+  // TODO entirely your job (except Circle) -- DONE!
 
   def apply(s: Shape): Location = s match {
     case Circle(radius) =>
       Location(-radius, -radius, Rectangle(2 * radius, 2 * radius))
     case Rectangle(width, height) =>
       Location(0, 0, Rectangle(width, height))
-    case Polygon(point1,point2,point3,point4) =>
-      Location(50, 50, Rectangle(70, 60))
-    case Location(width, height, shape) => Location(30, 80, Rectangle(470, 320))
-     // Location(width, height, shape)
-    case Fill(shape) =>
-      Location(0, 0, shape)
-    case Outline(shape) =>
-      Location(0, 0, shape)
-    case Stroke(color, shape) =>
-      Location(0, 0, shape)
-    case Group(shapeList) =>
-      Location(150, 50, Rectangle(350, 300))
-      /*for ( shape <- shapeList.seq ){
-        Location(0, 0, shape)
+    case Polygon(points @ _*) =>
+      var xmin = Int.MaxValue
+      var ymin = Int.MaxValue
+      var xmax = Int.MinValue
+      var ymax = Int.MinValue
+      def pointBounds(point: Point) {
+
+        xmin = Math.min(xmin, point.x)
+        ymin = Math.min(ymin, point.y)
+        xmax = Math.max(xmax, point.x)
+        ymax = Math.max(ymax, point.y)
+
       }
-      Location(0, 0, Rectangle(0, 0))*/
+      points.map(point => pointBounds(point))
+      xmax -= xmin
+      ymax -= ymin
+      Location(xmin, ymin, Rectangle(xmax, ymax))
+
+    case Location(width, height, shape) =>
+        var loc:Location = this.apply(shape)
+        val w = width + loc.x
+        val h = height + loc.y
+        Location(w, h,loc.child)
+
+    case Fill(shape) =>
+      this.apply(shape)
+
+    case Outline(shape) =>
+      this.apply(shape)
+
+    case Stroke(color, shape) =>
+      this.apply(shape)
+
+    case Group(shapeList) =>
+      var xmin = Int.MaxValue
+      var ymin = Int.MaxValue
+      var xmax = Int.MinValue
+      var ymax = Int.MinValue
+
+      def findBounds(shape: Shape) {
+        val loc = this.apply(shape)
+        xmin = Math.min(xmin, loc.x)
+        ymin = Math.min(ymin, loc.y)
+        var shRect = loc.child.asInstanceOf[Rectangle]
+        xmax = Math.max(xmax, shRect.width + loc.x)
+        ymax = Math.max(ymax, shRect.height + loc.y)
+      }
+
+      shapeList.map(shape =>findBounds(shape))
+      xmax -= xmin
+      ymax -= ymin
+      Location(xmin, ymin, Rectangle(xmax, ymax))
+
     case _ =>
-      Location(30, 80, Rectangle(470, 320))
-     // Location(0, 0, Rectangle(0, 0))
+      Location(0, 0, Rectangle(0, 0))
+
   }
+
 }
